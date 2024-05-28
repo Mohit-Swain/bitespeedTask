a83d17c3f870aa71a18aa4c293196a218fd1f8d0
Mohit Swain <swain6564@gmail.com>
2024-05-28 19:42:06 +0530

    added cors

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require('cors');
 
 var contactRouters = require('./routes/contactRouters');
 
 var app = express();
 
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
